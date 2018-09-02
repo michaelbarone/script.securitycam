e09d50683026f78949829ea94bba8270eea8eae7
Paulemann <markra@hotmail.de>
2018-09-02 11:30:36 +0200

    re-work
    Made code more consistent, removed unneccessary code and tried to make the main method run in a thread to support multple parallel cam previews

--- script.securitycam/default.py
+++ script.securitycam/default.py
@@ -1,124 +1,139 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Source: https://github.com/RyanMelenaNoesis/XbmcSecurityCamOverlayAddOn"
+# and kodi forum discussion: https://forum.kodi.tv/showthread.php?tid=182540
+#
+# JSONRPC Call to trigger this script:
+# e.g. http://192.168.178.12:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.securitycam"},"id":"1"}
+
 # Import the modules
-import os, time, urllib2, xbmc, xbmcaddon, xbmcgui, xbmcvfs
+import os, time, urllib2, xbmc, xbmcaddon, xbmcgui, xbmcvfs, random, string
+from threading import Thread
 
 # Constants
 ACTION_PREVIOUS_MENU = 10
 ACTION_BACKSPACE = 110
 ACTION_NAV_BACK = 92
-ADD_ON_ID = 'script.securitycam'
 
 # Set plugin variables
-__addon__    = xbmcaddon.Addon()
-__cwd__      = __addon__.getAddonInfo('path').decode("utf-8")
-__icon__     = xbmc.translatePath(os.path.join(__cwd__, 'icon.png').encode("utf-8")).decode("utf-8")
-__profile__  = xbmc.translatePath(__addon__.getAddonInfo('profile')).decode("utf-8")
-__resource__ = xbmc.translatePath(os.path.join(__cwd__, 'resources').encode("utf-8")).decode("utf-8")
-__snapshot_dir__ = xbmc.translatePath(os.path.join(__profile__, 'snapshots').encode("utf-8")).decode("utf-8")
+__addon__        = xbmcaddon.Addon()
+__addon_id__     = __addon__.getAddonInfo('id')
+__addon_path__   = __addon__.getAddonInfo('path')
+__profile__      = __addon__.getAddonInfo('profile')
+__icon__         = os.path.join(__addon_path__, 'icon.png')
+__spinner__      = os.path.join(__addon_path__, 'loading.gif')
 
 # Get settings
 url       = __addon__.getSetting('url')
 username  = __addon__.getSetting('username')
 password  = __addon__.getSetting('password')
-width     = int(float(__addon__.getSetting('width')))
-height    = int(float(__addon__.getSetting('height')))
-interval  = int(float(__addon__.getSetting('interval')))
-autoClose = (__addon__.getSetting('autoClose') == 'true')
-duration  = int(float(__addon__.getSetting('duration')) * 1000)
+
+_width     = int(float(__addon__.getSetting('width')))
+_height    = int(float(__addon__.getSetting('height')))
+_interval  = int(float(__addon__.getSetting('interval')))
+_autoClose = bool(__addon__.getSetting('autoClose') == 'true')
+_duration  = int(float(__addon__.getSetting('duration')) * 1000)
 
 # Utils
-
 def log(message,loglevel=xbmc.LOGNOTICE):
-    xbmc.log((ADD_ON_ID + ": " + message).encode('UTF-8','replace'),level=loglevel)
+    xbmc.log(msg='[{}] {}'.format(__addon_id__, message), level=loglevel)
 
 # Classes
 class CamPreviewDialog(xbmcgui.WindowDialog):
-    def __init__(self):
-        log('CamPreviewDialog Initialized \n', xbmc.LOGDEBUG)
+    def __init__(self, index, url, username, password):
         COORD_GRID_WIDTH = 1280
         COORD_GRID_HEIGHT = 720
-        scaledWidth = int(float(COORD_GRID_WIDTH) / self.getWidth() * width)
-        scaledHeight = int(float(COORD_GRID_HEIGHT) / self.getHeight() * height)
-        self.image = xbmcgui.ControlImage(COORD_GRID_WIDTH - scaledWidth, COORD_GRID_HEIGHT - scaledHeight, scaledWidth, scaledHeight, __icon__)
+
+        scaledWidth = int(float(COORD_GRID_WIDTH) / self.getWidth() * _width)
+        scaledHeight = int(float(COORD_GRID_HEIGHT) / self.getHeight() * _height)
+
+        if index < 1 or index > 4:
+            log('Window index of {} is outside of allowed range (1..4)'.format(index))
+            return
+
+        scaledX = COORD_GRID_WIDTH - scaledWidth - 25
+        scaledY = (index - 1) * scaledHeight + index * 25
+
+        self.url = url
+        self.username = username
+        self.password = password
+
+        randomname = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+        self.snapshotdir = os.path.join(__profile__, randomname)
+        if not xbmcvfs.exists(self.snapshotdir):
+            xbmcvfs.mkdir(self.snapshotdir)
+
+        self.image = xbmcgui.ControlImage(scaledX, scaledY, scaledWidth, scaledHeight, __spinner__, aspectRatio = 1)
         self.addControl(self.image)
-        self.image.setAnimations([('WindowOpen', 'effect=slide start=%d time=1000 tween=cubic easing=in'%(scaledWidth),), ('WindowClose', 'effect=slide end=%d time=1000 tween=cubic easing=in'%(scaledWidth),)])
 
-    def start(self, autoClose, duration, interval, url, destination):
-        log('CamPreviewDialog Started \n', xbmc.LOGDEBUG)
-        self.isRunning = bool(1)
-        snapshot = ''
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        self.show()
+        self.isRunning = True
+
         startTime = time.time()
-        while(not autoClose or (time.time() - startTime) * 1000 <= duration):
+
+        request = urllib2.Request(self.url)
+
+        if self.username and self.password:
+            base64str = base64.b64encode('{}:{}'.format(self.username, self.password))
+            request.add_header('Authorization', 'Basic {}'.format(base64str))
+
+        i=0
+        while(not _autoClose or (time.time() - startTime) * 1000 <= _duration):
+            #snapshot = os.path.join(self.snapshotdir, 'snapshot' + str(time.time()) + '.jpg' )
+            snapshot = os.path.join(self.snapshotdir, 'snapshot' + str(i) + '.jpg' )
+            i = i+1
+
+            try:
+                imgData = urllib2.urlopen(request).read()
+                file = xbmcvfs.File(snapshot, 'wb')
+                file.write(imgData)
+                file.close()
+
+            except Exception, e:
+                log(str(e))
+
             if xbmcvfs.exists(snapshot):
-                os.remove(snapshot)
+                self.image.setImage(snapshot, False)
 
-            snapshot = self.downloadSnapshot(url, destination)
+            xbmc.sleep(_interval)
 
-            if snapshot != '':
-                self.update(snapshot)
-
-            xbmc.sleep(interval)
             if not self.isRunning:
                 break
-        self.close()
 
-    def downloadSnapshot(self, url, destination):
-        log('Retreiving Image \n', xbmc.LOGDEBUG)
-        try:
-            imgData = urllib2.urlopen(url).read()
-            filename = snapshot = xbmc.translatePath( os.path.join( destination, 'snapshot' + str(time.time()) + '.jpg' ).encode("utf-8") ).decode("utf-8")
-            output = open(filename,'wb')
-            log('Saving Image To: [' + filename + ']\n', xbmc.LOGDEBUG)
-            output.write(imgData)
-            output.close()
-            return filename
-        except:
-            return ''
+        self.close()
+        self.cleanup()
+
+    def cleanup(self):
+        files = xbmcvfs.listdir(self.snapshotdir)[1]
+        for file in files:
+            xbmcvfs.delete(os.path.join(self.snapshotdir, file))
+        xbmcvfs.rmdir(self.snapshotdir)
 
     def onAction(self, action):
-        log('Received Action: ' + str(action.getId()) + '\n', xbmc.LOGDEBUG)
+        log('onAction')
         if action in (ACTION_PREVIOUS_MENU, ACTION_BACKSPACE, ACTION_NAV_BACK):
-            self.isRunning = bool(0)
-            self.close()
+            self.stop()
 
-    def update(self, image):
-        log('Updating Image \n', xbmc.LOGDEBUG)
-        self.image.setImage(image, bool(0))
-    
-# Main execution
+    def stop(self):
+            self.isRunning = False
 
-log('AutoClose: [' + str(autoClose) + ']\n', xbmc.LOGDEBUG)
-log('Duration: [' + str(duration) + ']\n', xbmc.LOGDEBUG)
-log('Interval: [' + str(interval) + ']\n', xbmc.LOGDEBUG)
-log('Width: [' + str(width) + ']\n', xbmc.LOGDEBUG)
-log('Height: [' + str(height) + ']\n', xbmc.LOGDEBUG)
-log('Original URL: [' + url + ']\n', xbmc.LOGDEBUG)
 
-# Add Basic Authentication Headers
-if (username is not None and username != ''):
-    passwordManager = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    passwordManager.add_password(None, url, username, password)
-    authhandler = urllib2.HTTPBasicAuthHandler(passwordManager)
-    opener = urllib2.build_opener(authhandler)
-    urllib2.install_opener(opener)
+if __name__ == '__main__':
+    camPreview1 = CamPreviewDialog(1, url, username, password)
+    #camPreview1.update()
+    camPreview1.start()
 
-# Replace URL agruments
-argCount = len(sys.argv)
-for i in xrange(1, argCount):
-    search = '{%d}'%(i - 1)
-    replace = sys.argv[i]
-    url = url.replace(search, replace)
 
-log('Final URL: [' + url + ']\n', xbmc.LOGDEBUG)
+    #camPreview1 = CamPreviewDialog(1, url, username, password).start()
 
-xbmcvfs.mkdir(__snapshot_dir__)
+    url2='http://192.168.178.13/cgi-bin/get_stream6'
+    camPreview2 = CamPreviewDialog(2, url2, username, password).start()
 
-camPreview = CamPreviewDialog()
-camPreview.show()
-camPreview.start(autoClose, duration, interval, url, __snapshot_dir__)
-del camPreview
-
-dirs, files = xbmcvfs.listdir(__snapshot_dir__)
-for file in files:
-    log('Delete remaining snapshot: [' + file + ']\n', xbmc.LOGDEBUG)
-    xbmcvfs.delete(os.path.join(__snapshot_dir__, file))
-
+    del camPreview1
+    del camPreview2
